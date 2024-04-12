a5f12ec21345f1fe7eff2528addad6183ea5497b
stefaN-N-N <stefanlepsanovic@gmail.com>
2024-04-12 13:23:09 +0200

    SN-8 Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Layout from './pages/Layout/Layout';
+import Login from './pages/Login/Login';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
         <Route path='/' element={<Layout />}>
           <Route path='home' element={<Home />} />
         </Route>
+        <Route path='/login' element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
